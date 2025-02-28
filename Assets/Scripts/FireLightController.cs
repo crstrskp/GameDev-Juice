c41abfb58c6bfea5be652c6ae5e1348105b3fdb5
crstrskp <jesper@tjorne.dk>
2025-02-28 16:15:39 +0100

    added more fx

---
+++ Assets/Scripts/FireLightController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireLightController : MonoBehaviour
+{
+    [SerializeField]
+    private Light m_light;
+    [SerializeField]
+    private int m_baseIntensity = 3;
+    private int i = 0;
+
+    void Update()
+    {
+        var data = 1 * Math.Sin((m_baseIntensity * Math.PI * 60 * i++ * 0.0004)) + (m_baseIntensity / 2);
+        if (i > 2500) i = 0;
+        
+        m_light.intensity = Mathf.Lerp(m_light.intensity, (float)data, Time.deltaTime);
+    }
+}
--- Assets/fxDemo.cs
+++ Assets/fxDemo.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 public class fxDemo : MonoBehaviour
 {
     public enum effectType { particle, vfxGraph }
+    public effectType fxType;
 
     [SerializeField] private GameObject fxPrefab; 
-    public effectType fxType;
 
     void Awake()
     {
