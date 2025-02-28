53b26f0ad81a162453a99745d4cf25cd24c0e03d
crstrskp <jesper@tjorne.dk>
2025-02-28 16:19:50 +0100

    added TMPro_Text to fx_demo prefab

--- Assets/fxDemo.cs
+++ Assets/fxDemo.cs
@@ -1,15 +1,19 @@
+using TMPro; 
 using UnityEngine;
 
 public class fxDemo : MonoBehaviour
 {
     public enum effectType { particle, vfxGraph }
     public effectType fxType;
-
+    private TMP_Text label; 
+    
     [SerializeField] private GameObject fxPrefab; 
 
     void Awake()
     {
+        label = GetComponentInChildren<TMP_Text>();
         fxPrefab.SetActive(false);
+        label.text = fxPrefab.name;
     }
     
     public void Play()
